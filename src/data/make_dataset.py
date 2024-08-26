c75294a849faca587f4ef3b96a021498886ca601
Taimoor Aleem <taimooraleem01@gmail.com>
2024-08-25 21:08:54 -0400

    Create seperate DataFrames for Accelerometer and Gyroscope data and run a loop to add data from files to the DataFrame

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -13,5 +13,22 @@ def extract_features(file_path):
     category = filename.split('-')[2].rstrip("123")
     return participant, label, category
 
-f = files[0]
-participant, label, category = extract_features(f)
+acc_df = pd.DataFrame()
+gyr_df = pd.DataFrame()
+
+acc_set = 1
+gyr_set = 1
+
+for f in files:
+    participant, label, category = extract_features(f)
+    
+    df = pd.read_csv(f)
+    df["participant"] = participant
+    df["label"] = label
+    df["category"] = category
+    
+    if "Accelerometer" in f:
+        acc_df = pd.concat([acc_df, df], ignore_index=True)
+    
+    elif "Gyroscope" in f:
+        gyr_df = pd.concat([gyr_df, df], ignore_index=True)
