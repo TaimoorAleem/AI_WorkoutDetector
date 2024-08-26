eea98a671d138b4739b8969bf09f27f3048c1267
Taimoor Aleem <taimooraleem01@gmail.com>
2024-08-26 09:36:44 -0400

    Fix bug in order of renaming columns

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -82,9 +82,9 @@ final_dataset.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
     "set"
 ]
 
@@ -96,9 +96,9 @@ sampling =  {
     'gyr_x': "mean",
     'gyr_y': "mean",
     'gyr_z': "mean",
+    'participant': "last",
     'label': "last",
     'category': "last",
-    'participant': "last",
     'set': "last"
 }
 
