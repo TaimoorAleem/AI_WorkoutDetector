399e391ea83022816604b7111a76533f4119918e
Taimoor Aleem <taimooraleem01@gmail.com>
2026-02-07 11:05:38 -0500

    Train and compare neural network, random forest, KNN, decision tree, and naive bayes models

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -24,21 +24,21 @@ df_train = df.drop(["participant", "category", "set"], axis=1)
 X = df_train.drop("label", axis=1)
 Y = df_train["label"]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
 
 fig, ax = plt.subplots(figsize=(10, 5))
 df_train["label"].value_counts().plot(
     kind="bar", title="Class distribution in the dataset", ax=ax, color=["lightblue"], label="Total"
 )
-Y_train.value_counts().plot(kind="bar", ax=ax, color=["dodgerblue"], label="Train")
-Y_test.value_counts().plot(kind="bar", ax=ax, color=["royalblue"], label="Test")
+y_train.value_counts().plot(kind="bar", ax=ax, color=["dodgerblue"], label="Train")
+y_test.value_counts().plot(kind="bar", ax=ax, color=["royalblue"], label="Test")
 plt.legend()
 plt.show()
 
 # Split feature subsets
 
-basic_features = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
-square_features = ["acc_r", "gyro_r"]
+basic_features = ["acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z"]
+square_features = ["acc_r", "gyr_r"]
 pca_features = ["pca_1", "pca_2", "pca_3"]
 time_features = [f for f in df_train.columns if "_temp_" in f]
 frequency_features = [f for f in df_train.columns if ("_freq_" in f) or ("_pse" in f)]
@@ -62,9 +62,11 @@ learner = ClassificationAlgorithms()
 
 max_features = 10
 selected_features, ordered_features, ordered_scores = learner.forward_selection(
-    max_features, X_train, Y_train
+    max_features, X_train, y_train
 )
 
+# Commented the results of the forward selection to avoid running it again, as it takes a long time to run.
+''' 
 selected_features = ['acc_y_freq_0.0_Hz_ws_10',
  'duration',
  'acc_x_freq_0.0_Hz_ws_10',
@@ -77,10 +79,179 @@ selected_features = ['acc_y_freq_0.0_Hz_ws_10',
  'acc_y_freq_2.5_Hz_ws_10'
  ]
 
+ordered_scores = [
+ 0.9268518518518518,
+ 0.9919753086419754,
+ 0.9990740740740741,
+ 0.9993827160493827,
+ 0.9996913580246913,
+ 0.9996913580246913,
+ 0.9996913580246913,
+ 0.9996913580246913,
+ 0.9996913580246913,
+ 0.9996913580246913
+]
+'''
+
 plt.figure(figsize=(10, 5))
 plt.plot(np.arange(1, max_features + 1, 1), ordered_scores)
 plt.xlabel("Number of features selected")
 plt.ylabel("Accuracy Score")
 plt.xticks(np.arange(1, max_features + 1, 1))
 plt.title("Forward Feature Selection")
+plt.show()
+
+# Grid search for best hyperparameters and model selection
+
+possible_feature_sets = [feature_set_1, feature_set_2, feature_set_3, feature_set_4, selected_features]
+
+feature_names = [
+    "Feature Set 1",
+    "Feature Set 2",
+    "Feature Set 3",
+    "Feature Set 4",
+    "Selected Features",
+]
+
+iterations = 1
+score_df = pd.DataFrame()
+
+for i, f in zip(range(len(possible_feature_sets)), feature_names):
+    print("Feature set:", i)
+    selected_train_X = X_train[possible_feature_sets[i]]
+    selected_test_X = X_test[possible_feature_sets[i]]
+
+    # First run non deterministic classifiers to average their score.
+    performance_test_nn = 0
+    performance_test_rf = 0
+
+    for it in range(0, iterations):
+        print("\tTraining neural network,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.feedforward_neural_network(
+            selected_train_X,
+            y_train,
+            selected_test_X,
+            gridsearch=False,
+        )
+        performance_test_nn += accuracy_score(y_test, class_test_y)
+
+        print("\tTraining random forest,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.random_forest(
+            selected_train_X, y_train, selected_test_X, gridsearch=True
+        )
+        performance_test_rf += accuracy_score(y_test, class_test_y)
+
+    performance_test_nn = performance_test_nn / iterations
+    performance_test_rf = performance_test_rf / iterations
+
+    # And we run our deterministic classifiers:
+    print("\tTraining KNN")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.k_nearest_neighbor(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_knn = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining decision tree")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.decision_tree(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_dt = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining naive bayes")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.naive_bayes(selected_train_X, y_train, selected_test_X)
+
+    performance_test_nb = accuracy_score(y_test, class_test_y)
+
+    # Save results to dataframe
+    models = ["NN", "RF", "KNN", "DT", "NB"]
+    new_scores = pd.DataFrame(
+        {
+            "model": models,
+            "feature_set": f,
+            "accuracy": [
+                performance_test_nn,
+                performance_test_rf,
+                performance_test_knn,
+                performance_test_dt,
+                performance_test_nb,
+            ],
+        }
+    )
+    score_df = pd.concat([score_df, new_scores])
+
+# Create a grouped bar plot to compare the results
+
+score_df.sort_values(by=["accuracy"], ascending=False)
+
+plt.figure(figsize=(10, 10))
+sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
+plt.title("Model performance comparison")
+plt.ylabel("Accuracy")
+plt.xlabel("Model")
+plt.ylim(0.7, 1)
+plt.legend(loc="lower right")
+plt.show()
+
+# Selecting RF as our best model and evaluate results
+
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y,
+) = learner.random_forest(
+    X_train[selected_features], y_train, X_test[selected_features], gridsearch=True
+)
+
+accuracy = accuracy_score(y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
 plt.show()
